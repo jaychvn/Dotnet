0c9a6c00886d59ce28df3c1ff2d108b45794a507
jaychvn <jaychvn22@gmail.com>
2020-01-29 19:24:41 +0000

    New Class Vehicle was introduced

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,7 +6,32 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
+            int myInt;
+            //Console.WriteLine(" myInt: {0}", myInt); 
+            myInt = 50_00_000;
+            Console.WriteLine(" myInt: {0}", myInt);
             Console.WriteLine("Hello World!");
+            var vehicle = new Vehicle("Toyota", "Auris", 2012);
+            Console.WriteLine($"My Vehicle: {vehicle}");
+        }
+    }
+
+    class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public Vehicle(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Make} {Model} {Year}";
         }
     }
 }
